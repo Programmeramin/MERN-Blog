d41ce8759c4aef4122bce242be0002cbfce40c8f
Md Amin Islam <142400970+Programmeramin@users.noreply.github.com>
2024-04-20 20:29:55 +0600

    create a header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Projects from "./pages/Projects"
 import Dashboard from "./pages/Dashboard"
+import Header from "./Component/Header"
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
          <Route path="/" element={<Home/>}/>
          <Route path="/about" element={<About/>}/>
-         <Route path="/signin" element={<SignIn/>}/>
-         <Route path="/signup" element={<SignUp/>}/>
+         <Route path="/sign-in" element={<SignIn/>}/>
+         <Route path="/sign-up" element={<SignUp/>}/>
          <Route path="/projects" element={<Projects/>}/>
          <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <h1 className='text-2xl'>Home</h1>
+    </>
   )
 }
 
