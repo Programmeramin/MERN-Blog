ad24a2afa018804138fd79401e31943f701a6c39
Md Amin Islam <142400970+Programmeramin@users.noreply.github.com>
2024-05-28 03:41:46 +0600

    create user model

--- api/index.js
+++ api/index.js
@@ -1,20 +1,22 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv"
+import  MongoDBConnect  from "../config/MongoDBConnect.js";
 
 // config
 dotenv.config();
 const app = express();
 
-// env var
+// env var 
 const PORT = process.env.PORT || 6060
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({extended : false}));
 
 // route
 
 app.listen(PORT, () =>{
+    MongoDBConnect();    
     console.log(`Server is running on port ${PORT}`.bgBlue.white);
 });
---
+++ model/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+
+// create mongoose schema
+const userSchema = new mongoose.Schema({
+
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+
+    email : {
+        type: String,
+        required : true,
+        unique : true,
+    },
+
+    password : {
+        type: String,
+        required : true,
+        unique : true,
+    },
+
+    status: {
+        type: Boolean,
+        default: true,
+      },
+      trash: {
+        type: Boolean,
+        default: false,
+      },
+
+},{timestamps : true});
+
+
+export default mongoose.model("User", userSchema)
